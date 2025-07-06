24c527e7e0eac86855b9464f3ff59c042ca0a72d
farukycgl <faruk.kuy@gmail.com>
2025-07-06 12:04:03 +0300

    input content dynamically listed and toggle applied

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaClipboardList } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
 import TodoItem from './TodoItem';
@@ -6,6 +6,42 @@ import TodoItem from './TodoItem';
 
 
 const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    const inputContext = {
+        id: todos.length + 1,
+        text: inputValue,
+        isComplete: false,
+    };
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = () => {
+
+        if (inputValue === "") return null;
+
+        setTodos([...todos, inputContext]);
+        setInputValue("");
+    };
+
+    const toggle = (id) => {
+        setTodos((prevTodos) => {
+           return prevTodos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, isComplete: !todo.isComplete };
+                }
+                return todo;
+            });
+        });
+    };
+
+    useEffect(() => {
+        console.log(todos);
+    }, [todos])
+
     return (
         <div className='place-self-center bg-white w-[450px] h-[600px] p-16 flex flex-col gap-5 rounded-xl'>
             {/* baslik kismi */}
@@ -13,21 +49,25 @@ const Todo = () => {
                 Todo App
             </h1>
 
-            {/* ekleme kismi */}
+            {/* gorev ekleme kismi */}
             <div className='flex items-center gap-2 bg-amber-400 rounded-full'>
                 <input
+                    onChange={handleChange}
+                    value={inputValue}
                     type="text"
                     className='border-none outline-none bg-transparent p-3 flex-1 placeholder:text-gray-500'
                     placeholder='Yeni bir görev gir'
                 />
                 <div className='bg-amber-600 h-full w-14 flex items-center justify-center rounded-r-full'>
-                    <CiCirclePlus className='size-10 cursor-pointer' />
+                    <CiCirclePlus onClick={handleSubmit} className='size-10 cursor-pointer' />
                 </div>
             </div>
 
             {/* listelenen gorevler */}
             <div className='mt-5'>
-                <TodoItem/>
+                {todos.map(todo => (
+                    <TodoItem key={todo.id} todo={todo} toggle={toggle}/>
+                ))}
             </div>
 
 
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { FaTrash } from "react-icons/fa";
-import { FaRegCircle } from "react-icons/fa";
+import { FaTrash, FaRegCircle } from "react-icons/fa";
+import { FaCircleCheck } from "react-icons/fa6";
 
 
+const TodoItem = (props) => {
+    const { todo, toggle } = props;
 
-const TodoItem = () => {
     return (
-        <div className='flex flex-row w-full items-center p-2 gap-2 border-b pb-4'>
-            <FaRegCircle className='text-green-600 size-5'/>
-            <p className='flex-1'>bu görev tamamlandı.</p>
-            <FaTrash className='text-red-600 size-5'/>
+        <div
+            className='flex flex-row w-full items-center px-2 py-4 gap-2 border-b pb-4 cursor-pointer'
+            onClick={() => toggle(todo.id)}
+        >
+            {
+                todo.isComplete ? (<FaCircleCheck className='text-green-600 size-5'/>)
+                    : (<FaRegCircle className='text-green-600 size-5' />
+                    )}
+            <p className='flex-1'>{todo.text}</p>
+            <FaTrash className='text-red-600 size-5 hover:scale-110 transition-all' />
 
         </div>
     );

ebb8342a1e8aa6acd48347078aa1e87e4d769e99
farukycgl <faruk.kuy@gmail.com>
2025-07-07 13:39:11 +0300

    handleDelete function has been added and localstorage has been integrated

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,78 +1,94 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { FaClipboardList } from "react-icons/fa";
-import { CiCirclePlus } from "react-icons/ci";
-import TodoItem from './TodoItem';
-
-
+import { FaPlus } from "react-icons/fa6";
+import TodoItem from "./TodoItem";
 
 const Todo = () => {
-    const [todos, setTodos] = useState([]);
-    const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
+  const [inputValue, setInputValue] = useState("");
 
-    const inputContext = {
-        id: todos.length + 1,
-        text: inputValue,
-        isComplete: false,
-    };
+  const inputContext = {
+    id: todos.length + 1,
+    text: inputValue,
+    isComplete: false,
+  };
 
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-    };
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    const handleSubmit = () => {
+  const handleSubmit = () => {
+    if (inputValue === "") return null;
 
-        if (inputValue === "") return null;
+    setTodos([...todos, inputContext]);
+    setInputValue("");
+  };
 
-        setTodos([...todos, inputContext]);
-        setInputValue("");
-    };
+  const handleDelete = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== id);
+    });
+  };
 
-    const toggle = (id) => {
-        setTodos((prevTodos) => {
-           return prevTodos.map((todo) => {
-                if (todo.id === id) {
-                    return { ...todo, isComplete: !todo.isComplete };
-                }
-                return todo;
-            });
-        });
-    };
+  const toggle = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isComplete: !todo.isComplete };
+        }
+        return todo;
+      });
+    });
+  };
 
-    useEffect(() => {
-        console.log(todos);
-    }, [todos])
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    return (
-        <div className='place-self-center bg-white w-[450px] h-[600px] p-16 flex flex-col gap-5 rounded-xl'>
-            {/* baslik kismi */}
-            <h1 className='text-3xl font-semibold tracking-wider flex gap-2 items-center'><FaClipboardList />
-                Todo App
-            </h1>
+  return (
+    <div className="place-self-center bg-[#F0E4D3] w-[450px] h-[600px] p-16 flex flex-col gap-5 rounded-xl">
+      {/* baslik kismi */}
+      <h1 className="text-3xl font-semibold tracking-wider flex gap-2 items-center">
+        <FaClipboardList />
+        Todo List
+      </h1>
 
-            {/* gorev ekleme kismi */}
-            <div className='flex items-center gap-2 bg-amber-400 rounded-full'>
-                <input
-                    onChange={handleChange}
-                    value={inputValue}
-                    type="text"
-                    className='border-none outline-none bg-transparent p-3 flex-1 placeholder:text-gray-500'
-                    placeholder='Yeni bir görev gir'
-                />
-                <div className='bg-amber-600 h-full w-14 flex items-center justify-center rounded-r-full'>
-                    <CiCirclePlus onClick={handleSubmit} className='size-10 cursor-pointer' />
-                </div>
-            </div>
-
-            {/* listelenen gorevler */}
-            <div className='mt-5'>
-                {todos.map(todo => (
-                    <TodoItem key={todo.id} todo={todo} toggle={toggle}/>
-                ))}
-            </div>
-
-
+      {/* gorev ekleme kismi */}
+      <div className="flex items-center gap-2 bg-[#FAF7F3] rounded-full">
+        <input
+          onChange={handleChange}
+          value={inputValue}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleSubmit();
+            };
+          }}
+          type="text"
+          className="border-none outline-none bg-transparent p-3 flex-1 placeholder:text-gray-500"
+          placeholder="Yeni bir görev gir"
+        />
+        <div className="bg-amber-600 h-full w-14 flex items-center justify-center rounded-r-full">
+          <FaPlus onClick={handleSubmit} className="size-10 cursor-pointer" />
         </div>
-    );
+      </div>
+
+      {/* listelenen gorevler */}
+      <div className="mt-5">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggle={toggle}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from "react";
 import { FaTrash, FaRegCircle } from "react-icons/fa";
 import { FaCircleCheck } from "react-icons/fa6";
 
-
 const TodoItem = (props) => {
-    const { todo, toggle } = props;
+  const { todo, toggle, handleDelete } = props;
 
-    return (
-        <div
-            className='flex flex-row w-full items-center px-2 py-4 gap-2 border-b pb-4 cursor-pointer'
-            onClick={() => toggle(todo.id)}
-        >
-            {
-                todo.isComplete ? (<FaCircleCheck className='text-green-600 size-5'/>)
-                    : (<FaRegCircle className='text-green-600 size-5' />
-                    )}
-            <p className='flex-1'>{todo.text}</p>
-            <FaTrash className='text-red-600 size-5 hover:scale-110 transition-all' />
-
-        </div>
-    );
-}
+  return (
+    <div
+      className="flex flex-row w-full items-center px-2 py-4 border-b border-b-amber-600 gap-2 pb-4 cursor-pointer select-none"
+      onClick={() => toggle(todo.id)}
+    >
+      {todo.isComplete ? (
+        <FaCircleCheck className="text-green-600 size-5" />
+      ) : (
+        <FaRegCircle className="text-green-600 size-5" />
+      )}
+      <p
+        className={`flex-1
+                ${todo.isComplete ? "line-through  decoration-black text-red-400" : ""}`}
+      >
+        {todo.text}
+      </p>
+      <FaTrash 
+      onClick={() => handleDelete(todo.id)}
+      className="text-red-600 size-5 hover:scale-110 transition-all" />
+    </div>
+  );
+};
 
 export default TodoItem;
